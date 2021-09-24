f30e9e92a36df058ffe1419032da583505267849
348048638 <348048638@qq.com>
2021-09-24 14:48:55 +0800

    'nothing'

--- src/components/MenuY.vue
+++ src/components/MenuY.vue
@@ -2,24 +2,24 @@
   <div id="MenuY">
     <ul id="MenuY_ul" v-on:click="OpenOrHidden">
       <li>
-        <div>Menu-1</div>
+        <div class="MenuY_ul_li_div">Menu-1</div>
         <ul>
           <li>
-            <div>Menu-1-1</div>
+            <div class="MenuY_ul_li_div">Menu-1-1</div>
           </li>
           <li>
-            <div>Menu-1-2</div>
+            <div class="MenuY_ul_li_div">Menu-1-2</div>
           </li>
           <li>
-            <div>Menu-1-3</div>
+            <div class="MenuY_ul_li_div">Menu-1-3</div>
           </li>
         </ul>
       </li>
       <li>
-        <div>Menu-2</div>
+        <div class="MenuY_ul_li_div">Menu-2</div>
       </li>
       <li>
-        <div>Menu-3</div>
+        <div class="MenuY_ul_li_div">Menu-3</div>
       </li>
     </ul>
   </div>
@@ -34,15 +34,25 @@ export default {
       console.log(e.target)
       console.log(e.currentTarget)
     }
+  },
+  mounted () {
+    let divs = document.getElementsByClassName('MenuY_ul_li_div')
+    for (let i = 0; i < divs.length; i++) {
+      let div = divs[i]
+      div.addEventListener('click', () => {
+        for (let o = 0; o < divs.length; o++) {
+          let node = divs[o]
+          node.style.color = 'black'
+          node.style.background = 'white'
+        }
+        div.style.color = 'white'
+        div.style.background = 'black'
+      })
+    }
   }
 }
 </script>
 
 <style>
-#MenuY_ul>li>div{
-  background: #FF6600;
-}
-li{
-  display: none;
-}
+
 </style>
